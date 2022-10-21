e3935a7bf4506c100db5761dfe8b774383912b61
luis.lins <luis.lins@linx.com.br>
2022-10-21 19:31:40 -0300

    reformulacao completa no board pra utilizar como um array e lógica de movimentacao de peças por tipo

--- src/Piece.tsx
+++ src/Piece.tsx
@@ -8,16 +8,6 @@ type PieceProps = {
 function Piece(props: PieceProps) {
   const [color, setColor] = useState("");
 
-  // function getClass(props: PieceProps) {
-  //   if (props.position.includes("8") || props.position.includes("7")) {
-  //     return "piece black";
-  //   } else if (props.position.includes("1") || props.position.includes("2")) {
-  //     return "piece white";
-  //   } else {
-  //     ("");
-  //   }
-  // }
-
   useEffect(() => {
     if (props.position.includes("8") || props.position.includes("7")) {
       setColor("black");
